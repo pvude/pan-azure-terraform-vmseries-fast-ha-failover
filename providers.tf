92bb5a194073f334e9aa50844752a75f576be520
Phong Vu <pvu@t-netpro.de>
2023-10-23 16:06:48 +0200

    new cred

--- providers.tf
+++ providers.tf
@@ -10,4 +10,9 @@ terraform {
 
 provider "azurerm" {
   features {}
+
+  subscription_id = "94242550-9519-4cbd-adc0-7c51ba75819a"
+  client_id       = "98e8cc68-eace-4485-abc4-cb7e80035b7c"
+  client_secret   = var.client_secret
+  tenant_id       = "48523430-b66b-4518-a54a-5e86d1e380be"
 }
